6147afb2ad10e43c5d7176ef2bb31e64ebd84f8d
Girish Sortur <girishsortur@gmail.com>
2017-04-25 09:43:43 +0530

    First commit

---
+++ src/main/test1.java
@@ -0,0 +1,40 @@
+package main;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class test1 {
+	private WebDriver driver;
+
+	@BeforeTest
+	public void initDriver() throws Exception {
+		System.out.println("Jenkins run started");
+		System.setProperty("webdriver.chrome.driver", "C:/Users/I335799/Desktop/jenkins/chromedriver_win32/chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+
+	@Test
+	public void test_auto() throws Exception {
+		final String searchKey = "Jenkins";
+		System.out.println("Search " + searchKey + " in google");
+		driver.navigate().to("http://www.google.com");
+		Thread.sleep(10);
+		WebElement element = driver.findElement(By.name("q"));
+		element.sendKeys(searchKey);
+		element.submit();
+		//(new WebDriverWait(driver, 10)).until(ExpectedConditions.invisibilityOfAllElements(driver.findElements(By.name("q"))));
+	}
+
+	@AfterTest
+	public void quitDriver() throws Exception {
+		driver.quit();
+	}
+}
